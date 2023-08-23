8bfc473d130ed826560818ad7c6330ebd849775b
Chatue Sobgoui Gildas <gildassob@gmail.com>
2023-08-23 12:01:42 +0100

    stable version

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const styles = {backgroundColor:"#7950f2",color:"#fff"}
+  let [step,setStep] = useState(1)
+
+  function handleClick(event){
+    const  {name} = event.target
+    if(name === "previous"){
+      if(step > 1) { setStep(currentStepState=>currentStepState-1)}
+     
+    }else if(name === "next"){
+      if(step < 3)  {setStep(currentStepState=>currentStepState+1)}
+    }
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">Step {step} : {messages[step - 1]}</p>
+      <div className="buttons">
+        <button name="previous" style={styles} onClick={e=>handleClick(e)}>Previous</button>
+        <button name="next" style={styles} onClick={e=>handleClick(e)}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
